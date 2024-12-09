8910f273f75f7ff444f3ce74c9c63915825fd50a
trancaothien <thiennh278612@gmail.com>
2024-12-09 23:48:40 +0700

    [Config] Update folder struct

---
+++ Strangers/Sources/Presentation/Modules/Navigation/NavigationView.swift
@@ -0,0 +1,19 @@
+//
+//  NavigationView.swift
+//  Strangers
+//
+//  Created by Trần Cao Thiên on 9/12/24.
+//  Copyright © 2024 Studio35. All rights reserved.
+//
+
+import SwiftUI
+
+struct NavigationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NavigationView()
+}
---
+++ Strangers/Sources/Presentation/Modules/NewFeed/NewFeedViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  NewFeedViewModel.swift
+//  Strangers
+//
+//  Created by Trần Cao Thiên on 9/12/24.
+//  Copyright © 2024 Studio35. All rights reserved.
+//
+
+import Foundation
